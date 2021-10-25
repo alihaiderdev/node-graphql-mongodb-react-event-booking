6c42b1ef50687c117e29209051464b42322229f1
alihaiderdev <ah1675179@gmail.com>
2021-10-25 11:06:17 +0500

    types, queries for getting data and mutations for creating, updating and deleting data

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+const PORT = 8080;
+
+const events = [];
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: (args) => {
+        const { title, description, price, date } = args.eventInput;
+        const event = {
+          _id: Math.random().toString(),
+          title,
+          description,
+          price: +price,
+          date,
+        };
+        events.push(event);
+        return event;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => console.log(`App is running at http://localhost:${PORT}/graphql`));
